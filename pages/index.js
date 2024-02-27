1d79a5e28c92c996049d5f8102093938e5402b08
ZHU LEI <zhulei010414@gmail.com>
2024-02-27 14:28:45 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -131,9 +131,16 @@ export default function Home() {
         )}
         <div className="mt-10 laptop:mt-40 p-2 laptop:p-0" ref={aboutRef}>
           <h1 className="tablet:m-10 text-2xl text-bold">About.</h1>
-          <p className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5">
-            {data.aboutpara}
-          </p>
+          <div className="tablet:m-10 mt-2 text-xl laptop:text-xl w-full laptop:w-3/5">
+              <p>Hey there🙋, I'm Lei Zhu, a recent graduate with a Bachelor of Engineering in Computer Science and Technology from University of Electronic Science and Technology of China (UESTC) 电子科技大学 and pursing my Master's degree in Information Science at UIUC.</p>
+              <p>Throughout my academic and professional journey, I have passionately pursued opportunities to expand my expertise in software development and data analysis, culminating in a series of internships and projects that have significantly honed my skills and prepared me for the challenges of the tech industry.</p>
+              <p>My adventure in the field of technology began shortly after my undergraduate studies at the University of Electronic Science and Technology of China, where I majored in Computer Science and Technology. This solid foundation equipped me with a deep understanding of data structures, algorithms, and software engineering principles, setting the stage for my future endeavors.</p>
+              <p>In the summer of 2021, I embarked on an enriching internship with China Mingsheng Banking Corp. Ltd in Chengdu, China, as a Financial Data Analyst. Here, I developed a data analysis system leveraging Python and R to enhance loan processing and credit risk assessment. This system utilized advanced machine learning models, including Random Forest and Logistic Regression, achieving an impressive 95% accuracy rate in assessing creditworthiness. This experience not only sharpened my analytical skills but also deepened my appreciation for the impactful integration of technology and finance.</p>
+              <p>My journey continued at Susallwave in Shenzhen, China, where I served as a Software Development Intern. During this time, I was instrumental in developing a web-based tool for efficient PDF file interaction, employing Java Spring Boot and React. This tool significantly improved operational efficiency, demonstrating my ability to blend frontend and backend development to create cohesive and user-friendly applications.</p>
+              <p>The pinnacle of my internships was my role as a Software Engineer Intern at DiDi Chuxing in Beijing, China. Here, I built a real-time system for driver and insurance data collection, integrating Apache Kafka for reliable data transmission and utilizing Java and Spring Boot. This project not only reduced driver insurance costs by 25% but also enhanced driver retention rates, showcasing my capacity to deliver solutions that have a tangible impact on business outcomes.</p>
+              <p>Parallel to my internships, I engaged in several projects that further exemplified my technical prowess and problem-solving capabilities. Notably, the Intelligent Profile project, a full-stack web application reminiscent of LinkedIn, underscored my proficiency in Java, Spring Boot, MySQL, and AWS. This project, along with others like the Scientific Translation Practice System and JD Comment Sentiment Analysis, underscore my versatility and adeptness across a broad spectrum of technologies and frameworks, including Python, TensorFlow, and MongoDB.</p>
+              <p>Welcome to my website, where I share my journey, experiences, and passion for all things. Let's connect and explore the exciting world of science and engineering together!</p>
+            </div>
         </div>
         <Footer />
       </div>
